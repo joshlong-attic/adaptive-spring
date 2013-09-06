9ef6b6c2f57e7af069cd2a134b5810e3c19eb4da
Josh Long <josh@joshlong.com>
2013-09-06 13:02:57 -0700

    adding an example of reparenting the context to produce the effect of "reloading" a bean.

---
+++ conditionals101/src/main/java/savetheenvironment/jmx/Main.java
@@ -0,0 +1,9 @@
+package savetheenvironment.jmx;
+
+/**
+ * TODO we need an example demonstrating how to manipulate the state of a bean using JMX
+ *
+ * @author Josh Long
+ */
+public class Main {
+}
