7b7a181f475fdc79bf7f3dc5ea0cf87464dbe6e7
Josh Long <josh@joshlong.com>
2013-09-07 20:44:26 -0700

    added JMX to the example.

--- conditionals/src/test/java/savetheenvironment/refresh/SwappableDataSourceConfiguration.java
+++ conditionals/src/test/java/savetheenvironment/refresh/SwappableDataSourceConfiguration.java
@@ -18,10 +18,15 @@ import java.sql.Driver;
 @Configuration
 @PropertySource("/config.properties")
 public class SwappableDataSourceConfiguration {
+
     public static final String DB_URL = "db.url";
+
     public static final String DB_DRIVER_CLASS = "db.driverClass";
+
     public static final String DB_USER = "db.user";
+
     public static final String DB_PASSWORD = "db.password";
+
     private final Log log = LogFactory.getLog(getClass());
 
     @Bean
@@ -45,7 +50,8 @@ public class SwappableDataSourceConfiguration {
                     String user = environment.getProperty(DB_USER);
                     Class<? extends Driver> driverClass = environment.getPropertyAsClass(DB_DRIVER_CLASS, Driver.class);
                     Driver newInstance = driverClass.newInstance();
-                    log.info(String.format("refreshing dataSource configuration with new values: url=%s, password=%s, user=%s", url, pw, user));
+                    log.info(String.format("refreshing dataSource configuration" +
+                            " with new values: url=%s, password=%s, user=%s", url, pw, user));
                     return new SimpleDriverDataSource(newInstance, url, user, pw);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
