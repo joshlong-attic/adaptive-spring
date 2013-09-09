cbe47223c0303f364027a7bcc5f13749c211979e
Kevin Nilson <kevin@javaclimber.com>
2013-09-09 14:46:17 -0700

    cleanup formatting

--- profilesmocking/src/main/java/savetheenvironment/embedded/video/VideoSearch.java
+++ profilesmocking/src/main/java/savetheenvironment/embedded/video/VideoSearch.java
@@ -4,9 +4,5 @@ package savetheenvironment.embedded.video;
 import java.util.List;
 
 public interface VideoSearch {
-
-
-
-
     public List<String> lookupVideo(String searchTerm) throws Exception;
 }
