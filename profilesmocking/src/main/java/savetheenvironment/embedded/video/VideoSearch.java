67a2b8b475e6dd62f178a56b475f4c47a7f3ba59
Kevin Nilson <kevin@javaclimber.com>
2013-09-09 14:21:08 -0700

    add simple example for showing using profiles.
    I want this in separate example so I can see both profiles running with run.sh

---
+++ profilesmocking/src/main/java/savetheenvironment/embedded/video/VideoSearch.java
@@ -0,0 +1,12 @@
+package savetheenvironment.embedded.video;
+
+
+import java.util.List;
+
+public interface VideoSearch {
+
+
+
+
+    public List<String> lookupVideo(String searchTerm) throws Exception;
+}
